d50a5dae33d458edcc4c82c4bee5349c9eb3000e
Robert Jones <robert.g.jones@gmail.com>
2018-05-01 17:02:51 +0100

    Style mobile first
    Include breakpoint for tablet or greater that reveals the repo
    description, which is otherwise hidden

--- src/RepoTable.js
+++ src/RepoTable.js
@@ -14,7 +14,8 @@ class RepoTable extends React.Component {
       "https://api.github.com/search/repositories?q=vue+language:javascript&sort=stars&order=desc"
     )
       .then(res => res.json())
-      .then(res => that.setState({ repos: [...res.items] }));
+      .then(res => that.setState({ repos: [...res.items] }))
+      .catch(err => console.error(err));
   }
 
   componentDidMount() {
@@ -22,20 +23,21 @@ class RepoTable extends React.Component {
   }
   render() {
     return (
-      <table>
-        <thead>
+      <table className="repo-table">
+        <caption>Most Starred Vue Repos</caption>
+        <thead className="table-head">
           <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Stars</th>
+            <th className="header">Name</th>
+            <th className="header bp-tablet">Description</th>
+            <th className="header stars">Stars</th>
           </tr>
         </thead>
         <tbody>
           {this.state.repos.map(repo => (
             <tr key={repo.id}>
-              <td>{repo.full_name}</td>
-              <td>{repo.description}</td>
-              <td>{repo.stargazers_count}</td>
+              <td className="row name">{repo.full_name}</td>
+              <td className="row description bp-tablet">{repo.description}</td>
+              <td className="row stars">{repo.stargazers_count}</td>
             </tr>
           ))}
         </tbody>
